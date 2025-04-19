edbec6389c9d8bd3db51d971f45768e5d6710f3a
Ayaan <ayaanrk04@gmail.com>
2025-04-19 17:06:25 -0500

    Delete annual-report-ai directory

--- annual-report-ai/BackEnd/embed_utils.py
+++
@@ -1,9 +0,0 @@
-#creates a vector database and holds them in the database like FAISS so when a question is asked , its retrieved
-
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-
-def create_vectorstore(chunks):
-    embedding = OpenAIEmbeddings()
-    vectorstore = FAISS.from_documents(chunks, embedding)
-    return vectorstore
